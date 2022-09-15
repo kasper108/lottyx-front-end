e79ec0ae7d3e6763e3348572bc035d58b611df93
Stanislovas Kasperiūnas <stanislovaskasperiunas@gmail.com>
2022-09-15 08:46:31 +0300

    Navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { UserComponent } from './user/user.component';
+import { UserComponent } from './components/user/user.component';
 import { AddressComponent } from './address/address.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    AddressComponent
+    AddressComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
